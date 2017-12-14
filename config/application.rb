c29ba128954748ea455963f2ba2a3c99a5f1daf7
Sophie Sjoberg <sophie.sjoberg@gmail.com>
2017-12-14 21:32:55 +0100

    Configure app file

--- config/application.rb
+++ config/application.rb
@@ -16,10 +16,12 @@ module CooperApi
   class Application < Rails::Application
     config.api_only = true
     config.middleware.insert_before 0, Rack::Cors do
-     allow do
-       origins '*'
-       resource '*', headers: :any, methods: [:get, :post, :put, :delete]
-     end
-   end
- end
+      allow do
+        origins '*'
+        resource '*', headers: :any, methods: [:get, :post, :put, :delete],
+                      expose: %w(access-token expiry token-type uid client),
+                      max_age: 0
+      end
+    end
+  end
 end
